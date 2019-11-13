c694ca4501058afe7a861c6cb1c9a6407535adec
Wes <wesleyayer92@gmail.com>
2019-11-13 14:46:17 -0500

    implements add_todo function

--- todo.py
+++ todo.py
@@ -1,1 +1,16 @@
-# this is my todo app
+# keep track of todos using a list
+todo_list = []
+
+# i need a way to add todos
+def add_todo(todo):
+    todo_list.append(todo)
+
+print(todo_list)
+add_todo("feed the cat")
+print(todo_list)
+# i need to be able to delete todos
+
+# i need to print my todos
+
+# show user main menu after every command
+
