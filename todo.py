986fe35bc0b583db8234c0add35b7e20769db6b7
Wes <wesleyayer92@gmail.com>
2019-11-13 15:06:15 -0500

    added delete_todo function

--- todo.py
+++ todo.py
@@ -5,11 +5,18 @@ todo_list = []
 def add_todo(todo):
     todo_list.append(todo)
 
-print(todo_list)
+# print(todo_list)
 add_todo("feed the cat")
-print(todo_list)
+# print(todo_list)
 # i need to be able to delete todos
-
+def delete_todo(index):
+    # del todo_list[index]
+    try:
+        todo_list.pop(index)
+    except IndexError:
+        print("Sorry, no todo at that index.")
+delete_todo(0)
+print(todo_list)
 # i need to print my todos
 
 # show user main menu after every command
