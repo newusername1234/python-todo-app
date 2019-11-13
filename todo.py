e50d5669cfa0a41b9f2b60ee75bf73ff64ad9b77
Wes <wesleyayer92@gmail.com>
2019-11-13 15:23:52 -0500

    add print_todos function

--- todo.py
+++ todo.py
@@ -1,6 +1,9 @@
 # keep track of todos using a list
 todo_list = []
-
+# i need to print my todos
+def print_todos():
+    for todo in todo_list:
+        print(todo)
 # i need a way to add todos
 def add_todo(todo):
     todo_list.append(todo)
@@ -15,9 +18,9 @@ def delete_todo(index):
         todo_list.pop(index)
     except IndexError:
         print("Sorry, no todo at that index.")
-delete_todo(0)
-print(todo_list)
-# i need to print my todos
+# delete_todo(0)
+print_todos()
+
 
 # show user main menu after every command
 
